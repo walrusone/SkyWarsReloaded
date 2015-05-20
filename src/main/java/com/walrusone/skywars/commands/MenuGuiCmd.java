368fd4fea36be2d33d2c4336b9d3515670998884
zRA1Nz <g4m3rx001@gmail.com>
2015-05-19 14:21:19 -1000

    Nullchecking 2.5.4
    Added NullChecker for /swr menu

--- src/main/java/com/walrusone/skywars/commands/MenuGuiCmd.java
+++ src/main/java/com/walrusone/skywars/commands/MenuGuiCmd.java
@@ -1,12 +1,14 @@
 package com.walrusone.skywars.commands;
 
+import org.bukkit.ChatColor;
+
 import com.walrusone.skywars.SkyWarsReloaded;
 import com.walrusone.skywars.game.GamePlayer;
 import com.walrusone.skywars.menus.LobbyMainMenu;
 import com.walrusone.skywars.menus.MainMenu;
 
 public class MenuGuiCmd extends BaseCmd { 
-	
+
 	public MenuGuiCmd() {
 		forcePlayer = true;
 		cmdName = "menu";
@@ -20,21 +22,30 @@ public class MenuGuiCmd extends BaseCmd {
 	public boolean run() {
 		boolean enabled = (SkyWarsReloaded.get().getConfig().getBoolean("gameVariables.lobbyMenuEnabled") || SkyWarsReloaded.get().getConfig().getBoolean("gameVariables.optionsMenuEnabled"));
 		if (enabled) {
-			GamePlayer gPlayer = SkyWarsReloaded.getPC().getPlayer(player.getUniqueId());
-			if (gPlayer.inGame()) {
-				if (SkyWarsReloaded.get().getConfig().getBoolean("gameVariables.optionsMenuEnabled")) {
-					new MainMenu(gPlayer);
+			String lobby = SkyWarsReloaded.get().getConfig().getString("spawn.world");
+			if (lobby != null) {
+				GamePlayer gPlayer = SkyWarsReloaded.getPC().getPlayer(player.getUniqueId());
+				if (gPlayer.inGame()) {
+					if (SkyWarsReloaded.get().getConfig().getBoolean("gameVariables.optionsMenuEnabled")) {
+						new MainMenu(gPlayer);
+					}
+					return true;
+				} else {
+					String world = player.getWorld().getName();
+					String lobbyWorld = SkyWarsReloaded.get().getConfig().getString("spawn.world");
+					if (SkyWarsReloaded.get().getConfig().getBoolean("gameVariables.lobbyMenuEnabled") && world.equalsIgnoreCase(lobbyWorld)) {
+						new LobbyMainMenu(gPlayer);
+					}
+					return true;
 				}
-				return true;
 			} else {
-				String world = player.getWorld().getName();
-				String lobbyWorld = SkyWarsReloaded.get().getConfig().getString("spawn.world");
-				if (SkyWarsReloaded.get().getConfig().getBoolean("gameVariables.lobbyMenuEnabled") && world.equalsIgnoreCase(lobbyWorld)) {
-					new LobbyMainMenu(gPlayer);
-				}
+				sender.sendMessage(ChatColor.RED + "You need to have a lobby spawn set!");
 				return true;
 			}
-		} return false;
+		} else {
+			sender.sendMessage(ChatColor.RED + "Menu is disabled!");
+			return true;
+		}
 	}
 
 }
